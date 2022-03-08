312782f9abcaf5b353f3afa58a50e032e1f3b1d7
justin-henley <justin.a.henley@gmail.com>
2022-03-08 14:35:07 +0800

    Index is now the api endpoint inside dishes

--- api/dishes/index.php
+++ api/dishes/index.php
@@ -1,1 +1,29 @@
-<h1>Work in progress</h1>
+<?php
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+// Get the request method
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+// CORS solution
+if ($requestMethod === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+}
+
+// Choose the operation to carry out
+switch ($requestMethod) {
+    case 'GET':
+        // Search for a single result by dish id
+        if (isset($_GET['dishId'])) require './read_single.php';
+        // Search for any results like the given foreign name
+        else if (isset($_GET['nameForeign'])) require './search_foreign.php';
+        // Search for any results like the given chinese name
+        else if (isset($_GET['nameZHTW'])) require './search_zhtw.php';
+        // Search for all results without including translations, possibly filtering by category and meat type
+        else require './read.php';
+        break;
+    default:
+        echo json_encode(['message' => 'Invalid request method.']);
+}
