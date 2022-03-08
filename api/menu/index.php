ea7e85e3c14e9f165b62b417ca905f7ed1a8ad16
justin-henley <justin.a.henley@gmail.com>
2022-03-08 22:54:37 +0800

    menu index endpoint written

--- api/menu/index.php
+++ api/menu/index.php
@@ -1,1 +1,27 @@
-<h1>Work in progress</h1>
+<?php
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+// Get the request method
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+// CORS solution
+if ($requestMethod === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+}
+
+// If using this with a front-end causes trouble add an else before the switch
+// Choose the operation to carry out
+switch ($requestMethod) {
+    case 'GET':
+        // Search for a single menu item by restaurant id and dish id
+        if (isset($_GET['restaurantId']) && isset($_GET['dishId']))
+            require './read_single.php';
+        // Search for all results in a given language
+        else require './read.php';
+        break;
+    default:
+        echo json_encode(['message' => 'Invalid request method.']);
+}
