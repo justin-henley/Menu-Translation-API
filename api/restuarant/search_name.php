df1666dcca2aa36c0e75a84f7a7939c5be16a5c5
justin-henley <justin.a.henley@gmail.com>
2022-03-08 21:07:31 +0800

    renamed file

--- api/restuarant/search_name.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Searches for all restaurants like the given name in the given language, or in all languages
-// Params: restaurantName (req), languageId (opt)
-// Returns: restaurant id, name in given language and chinese, possibly all names in all languages
