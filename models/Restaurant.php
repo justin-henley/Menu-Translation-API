e113f86f9c50b0ab04624adb3ff573f1ed35ffba
justin-henley <justin.a.henley@gmail.com>
2022-03-08 16:58:11 +0800

    Finished Restaurant model

--- models/Restaurant.php
+++ models/Restaurant.php
@@ -6,6 +6,7 @@ class Restaurant
 
     // Properties
     public $restaurantId;
+    public $restaurantName;
     public $languageId;
 
     /**
@@ -33,6 +34,9 @@ class Restaurant
         // Prepare the statement
         $stmt = $this->connection->prepare($query);
 
+        // Clean data
+        $this->languageId = htmlspecialchars(strip_tags($this->languageId));
+
         // Bind parameters
         $stmt->bindValue(':languageId', $this->languageId);
 
@@ -45,7 +49,39 @@ class Restaurant
     // Reads a given restaurants name in the given language, or in all languages if none specified
     public function readSingle()
     {
-        return null;
+        // Return early if no id provided
+        if (!$this->restaurantId) return null;
+
+
+        // Clean data
+        $this->languageId = htmlspecialchars(strip_tags($this->languageId));
+        $this->restaurantId = htmlspecialchars(strip_tags($this->restaurantId));
+
+        // Create a filter for language if necessary
+        $lang = ($this->languageId)
+            ? " AND rest_names.languageId = :languageId"
+            : "";
+
+        // Create query
+        $query =
+            "SELECT
+                rest_names.name AS restaurantName,
+                rest_names.languageId
+            FROM restaurants
+            INNER JOIN rest_names ON restaurants.id = rest_names.restaurantId
+            WHERE restaurants.id = :restaurantId {$lang}";
+
+        // Prepare the statement
+        $stmt = $this->connection->prepare($query);
+
+        // Bind parameters
+        $stmt->bindValue(':restaurantId', $this->restaurantId);
+        if ($this->languageId) $stmt->bindValue(':languageId', $this->languageId);
+
+        // Execute the statement
+        $stmt->execute();
+
+        return $stmt;
     }
 
     // Searches for all restaurants like the given name in the given language, or in all languages
@@ -53,6 +89,37 @@ class Restaurant
     // Returns: restaurant id, name in given language and chinese, possibly all names in all languages
     public function search()
     {
-        return null;
+        // Return early if no name provided
+        if (!$this->restaurantName) return null;
+
+        // Clean data
+        $this->languageId = htmlspecialchars(strip_tags($this->languageId));
+        $this->restaurantName = htmlspecialchars(strip_tags($this->restaurantName));
+
+        // Create a filter for language if necessary
+        $lang = ($this->languageId)
+            ? " AND rest_names.languageId = :languageId"
+            : "";
+
+        // Create query
+        $query =
+            "SELECT
+                rest_names.name AS restaurantName,
+                rest_names.languageId
+            FROM restaurants
+            INNER JOIN rest_names ON restaurants.id = rest_names.restaurantId
+            WHERE rest_names.name LIKE :restaurantName {$lang}";
+
+        // Prepare the statement
+        $stmt = $this->connection->prepare($query);
+
+        // Bind parameters
+        $stmt->bindValue(':restaurantName', "%" . $this->restaurantName . "%");
+        if ($this->languageId) $stmt->bindValue(':languageId', $this->languageId);
+
+        // Execute statement
+        $stmt->execute();
+
+        return $stmt;
     }
 }
